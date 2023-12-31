5f67c83afade41bf8afc3b250b4c00f39f1769f4
Nandy Bâ <nandyba@yahoo.fr>
2023-12-31 15:32:16 +0100

    feature: update badge design and show it when realtime mode is enable

--- src/components/assetsView/indicators/RealtimeIndicator.tsx
+++ src/components/assetsView/indicators/RealtimeIndicator.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 
-import { Button, Indicator } from '@mantine/core'
+import { Badge } from '@mantine/core'
 
 import { selectUserRentCalculation } from 'src/store/features/settings/settingsSelector'
 import { RentCalculation } from 'src/types/RentCalculation'
@@ -13,17 +13,13 @@ export const RealtimeIndicator: FC = () => {
   })
   const rentCalculation = useSelector(selectUserRentCalculation)
 
+  if (rentCalculation !== RentCalculation.Realtime) return null;
+
   return (
     <div>
-      <Indicator
-        inline={true}
-        color={'red'}
-        disabled={rentCalculation !== RentCalculation.Realtime}
-      >
-        <Button variant={'subtle'} compact={true}>
-          {t('realtime')}
-        </Button>
-      </Indicator>
+      <Badge key='realtime' leftSection='⏰'>
+        {t('realtime')}
+      </Badge>
     </div>
   )
 }
